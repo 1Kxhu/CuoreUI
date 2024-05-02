58cd3e22efdd87fd1908a44473fcd08fa8b35f74
itzkxhu <kacperdkacperd@gmail.com>
2024-05-02 13:16:49 +0200

    Minor update. - Added GetRefreshRate() in helper's win32 class (public) - Added a spinner, that stays consistent on every refresh rate (by interpolation)

---
+++ wfcl1/Controls/cuiSpinner.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CuoreUI.Controls
+{
+    public partial class cuiSpinner : UserControl
+    {
+        Timer updateTimer = new Timer();
+        Timer rotationTimer = new Timer();
+
+        private float privateRotateSpeed = 2;
+        public float RotateSpeed
+        {
+            get
+            {
+                return privateRotateSpeed;
+            }
+            set
+            {
+                privateRotateSpeed = value;
+                Invalidate();
+            }
+        }
+
+        public bool RotateEnabled = true;
+
+        private float FloatLimitHalf = float.MaxValue / 2;
+
+        public cuiSpinner()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+
+            backsiteSmoothRotation = RotateSpeed;
+            Rotation = 0;
+
+            updateTimer.Interval = 500 / Helper.Win32.GetRefreshRate();
+            updateTimer.Tick += (obj, args) =>
+            {
+                Invalidate();
+            };
+
+            updateTimer.Start();
+
+            rotationTimer.Interval = 4;
+            rotationTimer.Tick += (obj, args) =>
+            {
+                // weird workaround because of interpolating Rotation (check SmoothRotation variable)
+                if (Rotation >= FloatLimitHalf)
+                {
+                    Rotation = 0;
+                }
+                if (RotateEnabled)
+                {
+                    Rotation += RotateSpeed;
+                }
+            };
+
+            rotationTimer.Start();
+        }
+
+        private Color privateArcColor = Color.White;
+        public Color ArcColor
+        {
+            get
+            {
+                return privateArcColor;
+            }
+            set
+            {
+                privateArcColor = value;
+                Invalidate();
+            }
+        }
+
+        private float privateRotation = 0;
+        public float Rotation
+        {
+            get
+            {
+                return privateRotation;
+            }
+            set
+            {
+                backsiteSmoothRotation = value;
+                privateRotation = value;
+                Invalidate();
+            }
+        }
+
+        private float backsiteSmoothRotation = 0;
+        private float SmoothRotation
+        {
+            get
+            {
+                // interpolate or whatever it's called to see the spinner spinning smoothly on all refresh rates
+                backsiteSmoothRotation = ((backsiteSmoothRotation * 14) + Rotation) / 15;
+                if (backsiteSmoothRotation > FloatLimitHalf)
+                {
+                    backsiteSmoothRotation = 0;
+                    Rotation = 0;
+                }
+
+                if (backsiteSmoothRotation > 360 && Rotation > 360)
+                {
+                    Rotation = 360 - backsiteSmoothRotation;
+                }
+                return backsiteSmoothRotation;
+            }
+        }
+
+        float ArcSize = 5;
+        float ArcDegrees = 90;
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            float SpinnerThickness = ArcSize * 2f;
+
+            RectangleF ClientConsideringArcSize = ClientRectangle;
+            ClientConsideringArcSize.Width = Math.Min(ClientConsideringArcSize.Width, ClientConsideringArcSize.Height);
+            ClientConsideringArcSize.Width = Math.Max(SpinnerThickness * 2 + (ArcSize * 2), ClientConsideringArcSize.Width);
+            ClientConsideringArcSize.Height = ClientConsideringArcSize.Width;
+            ClientConsideringArcSize.Inflate(-SpinnerThickness, -SpinnerThickness);
+
+            Pen ArcPen = new Pen(ArcColor, SpinnerThickness);
+            ArcPen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            ArcPen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+            e.Graphics.DrawArc(ArcPen, ClientConsideringArcSize, SmoothRotation, ArcDegrees);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            Rotation = 0;
+            backsiteSmoothRotation = 0;
+        }
+    }
+}
--- wfcl1/Helper.cs
+++ wfcl1/Helper.cs
@@ -1,5 +1,6 @@
 ﻿using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.Runtime.InteropServices;
 
 namespace CuoreUI
 {
@@ -67,6 +68,65 @@ namespace CuoreUI
             return path;
         }
 
+        public static class Win32
+        {
+            // Define the necessary structures and Win32 API functions
+            [StructLayout(LayoutKind.Sequential)]
+            public struct DEVMODE
+            {
+                private const int CCHDEVICENAME = 32;
+                private const int CCHFORMNAME = 32;
+                [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCHDEVICENAME)]
+                public string dmDeviceName;
+                public short dmSpecVersion;
+                public short dmDriverVersion;
+                public short dmSize;
+                public short dmDriverExtra;
+                public int dmFields;
+                public int dmPositionX;
+                public int dmPositionY;
+                public int dmDisplayOrientation;
+                public int dmDisplayFixedOutput;
+                public short dmColor;
+                public short dmDuplex;
+                public short dmYResolution;
+                public short dmTTOption;
+                public short dmCollate;
+                [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCHFORMNAME)]
+                public string dmFormName;
+                public short dmLogPixels;
+                public int dmBitsPerPel;
+                public int dmPelsWidth;
+                public int dmPelsHeight;
+                public int dmDisplayFlags;
+                public int dmDisplayFrequency;
+                public int dmICMMethod;
+                public int dmICMIntent;
+                public int dmMediaType;
+                public int dmDitherType;
+                public int dmReserved1;
+                public int dmReserved2;
+                public int dmPanningWidth;
+                public int dmPanningHeight;
+            }
 
+            [DllImport("user32.dll")]
+            public static extern bool EnumDisplaySettings(string deviceName, int modeNum, ref DEVMODE devMode);
+
+            public static int GetRefreshRate()
+            {
+                DEVMODE devMode = new DEVMODE();
+                devMode.dmSize = (short)Marshal.SizeOf(typeof(DEVMODE));
+
+                if (EnumDisplaySettings(null, -1, ref devMode))
+                {
+                    return devMode.dmDisplayFrequency;
+                }
+                else
+                {
+                    return 60;
+                }
+            }
+        }
     }
 }
