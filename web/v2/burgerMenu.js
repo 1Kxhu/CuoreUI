c71ab9931b412759504f5d49b0022212d16ac83f
itzkxhu <115172127+1Kxhu@users.noreply.github.com>
2024-11-01 22:22:28 +0100

    better performance + better mobile support

---
+++ web/v2/burgerMenu.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const navLinks = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        console.log('burger clicked');
+
+        navLinks.classList.toggle('nav-links-show');
+        burger.classList.toggle('burger-toggled');
+    });
+
+    const downloadButtonInNav = document.querySelector('.download-nav');
+    downloadButtonInNav.addEventListener('click', () => {
+        console.log('download clicked');
+        downloadButtonInNav.textContent = 'Redirecting..';
+
+    });
+});
